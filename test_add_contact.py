6572b6e4481bcf5120e8b790e29643e415ff317a
Ирина <Irina120490@yandex.ru>
2021-04-13 17:49:53 +0300

    contacts_1.16 Автотест для создания нового контакта New

--- test_add_contact.py
+++ test_add_contact.py
@@ -17,7 +17,7 @@ class TestAddContact(unittest.TestCase):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.add_new_contact(wd, Contactsgroup (firstname="Anna", middlename="Morozova", nickname="Ann",
+        self.add_new_contact(wd, Contact (firstname="Anna", middlename="Morozova", nickname="Ann",
                              company="Yandex", address="Rubin st.", home="122222", mobile="123333",
                              work="124444", fax="124563", email="Anna@mail.ru", email2="An@mail.ru",
                              bday="12", bmonth="March", byear="1993", address2="kjgekg", phone2="654321",
@@ -29,7 +29,7 @@ class TestAddContact(unittest.TestCase):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.add_new_contact(wd, Contactsgroup (firstname="Jonn", middlename="Smith", nickname="Jo", company="Google",
+        self.add_new_contact(wd, Contact (firstname="Jonn", middlename="Smith", nickname="Jo", company="Google",
                              address="Baker st.", home="1234567", mobile="1234565", work="1234545",
                              fax="1234758", email="Jonn@mail.ru", email2="Jo@mail.ru", bday="1", bmonth="January",
                              byear="1999", address2="asdfgh", phone2="123456", notes="jkl"))
