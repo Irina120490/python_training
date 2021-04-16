28efe9376698e52338323c53920a03aed1e52896
Ирина <Irina120490@yandex.ru>
2021-04-16 21:01:52 +0300

    python_training_2.1 Для контактов

--- test_add_contact.py
+++ test_add_contact.py
@@ -15,25 +15,21 @@ class TestAddContact(unittest.TestCase):
 
     def test_add_contact_two(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.add_new_contact(wd, Contact (firstname="Anna", middlename="Morozova", nickname="Ann",
                              company="Yandex", address="Rubin st.", home="122222", mobile="123333",
                              work="124444", fax="124563", email="Anna@mail.ru", email2="An@mail.ru",
                              bday="12", bmonth="March", byear="1993", address2="kjgekg", phone2="654321",
                              notes="rtyui"))
-        self.return_to_the_home_page(wd)
         self.logout(wd)
     
     def test_add_contact(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.add_new_contact(wd, Contact (firstname="Jonn", middlename="Smith", nickname="Jo", company="Google",
                              address="Baker st.", home="1234567", mobile="1234565", work="1234545",
                              fax="1234758", email="Jonn@mail.ru", email2="Jo@mail.ru", bday="1", bmonth="January",
                              byear="1999", address2="asdfgh", phone2="123456", notes="jkl"))
-        self.return_to_the_home_page(wd)
         self.logout(wd)
 
     def logout(self, wd):
@@ -93,6 +89,7 @@ class TestAddContact(unittest.TestCase):
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
 
     def login(self, wd, username, password):
+        self.open_home_page(wd)
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").clear()
