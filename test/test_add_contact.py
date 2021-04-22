49e952c617c1f64a5dd5a7c1b9a64576cc6ca99a
Ирина <Irina120490@yandex.ru>
2021-04-22 14:57:29 +0300

    python_training_3.3

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -2,18 +2,14 @@
 from model.contact import Contact
 
 def test_add_contact_two(app):
-    app.session.login(username="admin", password="secret")
     app.contact.add_new_contact(Contact(firstname="Anna", middlename="Morozova", nickname="Ann",
                                      company="Yandex", address="Rubin st.", home="122222", mobile="123333",
                                      work="124444", fax="124563", email="Anna@mail.ru", email2="An@mail.ru",
                                      bday="12", bmonth="March", byear="1993", address2="kjgekg", phone2="654321",
                                      notes="rtyui"))
-    app.session.logout
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.add_new_contact(Contact(firstname="Jonn", middlename="Smith", nickname="Jo", company="Google",
                                      address="Baker st.", home="1234567", mobile="1234565", work="1234545",
                                      fax="1234758", email="Jonn@mail.ru", email2="Jo@mail.ru", bday="1",
                                      bmonth="January", byear="1999", address2="asdfgh", phone2="123456", notes="jkl"))
-    app.session.logout
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,6 +2,4 @@
 from model.contact import Contact
 
 def test_modify_contact_name(app):
-    app.session.login(username="admin", password="secret")
     app.contact.modify_first_contact(Contact(firstname="Eng", middlename="Seckin", nickname="Ool"))
-    app.session.logout()
