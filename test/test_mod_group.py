9217a162a3bb13b773a700a7e6bb5cdc315c4566
Ирина <Irina120490@yandex.ru>
2021-04-24 16:10:11 +0300

    python_training_3.4(2)

--- test/test_mod_group.py
+++
@@ -1,7 +0,0 @@
-
-from model.group import Group
-
-def test_modify_group(app):
-    app.session.login(username="admin", password="secret")
-    app.group.modify_group(Group (name="abc", header="def", footer="ghi"))
-    app.session.logout()
