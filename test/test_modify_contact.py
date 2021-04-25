ce8a7f9bd3d8f3577289ec8a5c509831b09e6c35
Ирина <Irina120490@yandex.ru>
2021-04-25 23:28:52 +0300

    python_training_3.5(2)

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,4 +2,6 @@
 from model.contact import Contact
 
 def test_modify_contact_firstname(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="Seckin"))
     app.contact.modify_first_contact(Contact(firstname="Setiner"))
