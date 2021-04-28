169de9f67fbf5e1aa9d330e29ee4a44768e880fb
Ирина <Irina120490@yandex.ru>
2021-04-29 01:15:35 +0300

    python_training_4.10(3)

--- model/contact.py
+++ model/contact.py
@@ -1,4 +1,5 @@
 
+from sys import maxsize
 class Contact:
 
     def __init__(self, firstname=None, lastname=None, nickname=None, company=None, address=None, home=None,
@@ -24,7 +25,13 @@ class Contact:
         self.id = id
 
     def __repr__(self):
-        return "%s:%s" % (self.id, self.firstname)
+        return "%s:%s" % (self.id, self.lastname)
 
     def __eq__(self, other):
-        return self.id == other.id and self.firstname == other.firstname
+        return (self.id is None or other.id is None or self.id == other.id) and self.lastname == other.lastname
+
+    def id_or_max(self):
+        if self.id:
+            return int(self.id)
+        else:
+            return maxsize
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,15 +3,18 @@ from model.contact import Contact
 
 def test_add_contact_two(app):
     old_contacts = app.contact.get_contact_list()
-    app.contact.create(Contact(firstname="Anna", lastname="Morozova", nickname="Ann",
+    contact = Contact(firstname="Anna", lastname="Morozova", nickname="Ann",
                                      company="Yandex", address="Rubin st.", home="122222", mobile="123333",
                                      work="124444", fax="124563", email="Anna@mail.ru", email2="An@mail.ru",
                                      bday="12", bmonth="March", byear="1993", address2="kjgekg", phone2="654321",
-                                     notes="rtyui"))
+                                     notes="rtyui")
+    app.contact.create(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) + 1 == len(new_contacts)
+    old_contacts.append(contact)
+    assert old_contacts == new_contacts
 
-def test_add_contact(app):
+#def test_add_contact(app):
     old_contacts = app.contact.get_contact_list()
     app.contact.create(Contact(firstname="Jonn", lastname="Smith", nickname="Jo", company="Google",
                                      address="Baker st.", home="1234567", mobile="1234565", work="1234545",
