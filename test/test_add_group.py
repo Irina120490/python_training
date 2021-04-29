6a237020138ebf252d3303e9c642538bcaa1229c
Ирина <Irina120490@yandex.ru>
2021-04-30 01:25:04 +0300

    python_training_4.11(2)

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,9 +5,9 @@ def test_add_empty_group(app):
     old_groups = app.group.get_group_list()
     group = Group (name="", header="", footer="")
     app.group.create(group)
-    new_groups = app.group.get_group_list()
     #проверка
-    assert len(old_groups) + 1 == len(new_groups)
+    assert len(old_groups) + 1 == app.group.count()
+    new_groups = app.group.get_group_list()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
