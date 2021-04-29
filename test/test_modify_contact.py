4992302d822d80e2376f190f49d32a4aa7c63737
Ирина <Irina120490@yandex.ru>
2021-04-29 14:24:53 +0300

    python_training_4.10(6)

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -5,6 +5,10 @@ def test_modify_contact_firstname(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(firstname="Seckin"))
     old_contacts = app.contact.get_contact_list()
-    app.contact.modify_first_contact(Contact(firstname="Setiner"))
+    contact = Contact(firstname="Setiner")
+    contact.id = old_contacts[0].id
+    app.contact.modify_first_contact(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
+    old_contacts[0] = contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
