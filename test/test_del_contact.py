1151bab721fa3c1d7270503591a12945b070759c
Ирина <Irina120490@yandex.ru>
2021-04-29 18:14:59 +0300

    python_training_4.10(7)

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -3,7 +3,7 @@ from model.contact import Contact
 
 def test_delete_first_contact(app):
     if app.contact.count() == 0:
-        app.contact.create(Contact(lastname="Setiner"))
+        app.contact.create(Contact(firstname="Setiner"))
     old_contacts = app.contact.get_contact_list()
     app.contact.delete_first_contact()
     new_contacts = app.contact.get_contact_list()
